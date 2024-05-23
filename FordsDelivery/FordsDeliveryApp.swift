b63377caa6510d10f3c5fdf88dbefbe6fb70b3cd
cwalter50 <walt50@gmail.com>
2024-05-23 13:04:29 -0400

    Initial Commit

--- FordsDelivery/FordsDeliveryApp.swift
+++ FordsDelivery/FordsDeliveryApp.swift
@@ -5,13 +5,40 @@
 //  Created by Christopher Walter on 5/23/24.
 //
 
+//import SwiftUI
+//
+//@main
+//struct FordsDeliveryApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
+
+
+
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct FordsDeliveryApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
