2fa005d88d116186ad37ae39fbbe32c0d172169b
cwalter50 <walt50@gmail.com>
2024-05-23 12:39:49 -0400

    Initial Commit

---
+++ FordsDelivery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FordsDelivery
+//
+//  Created by Christopher Walter on 5/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FordsDelivery/FordsDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FordsDeliveryApp.swift
+//  FordsDelivery
+//
+//  Created by Christopher Walter on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FordsDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
